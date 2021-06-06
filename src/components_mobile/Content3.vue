73f142f2384e63854a158fe3902d1e3fdc4a4d10
현준 이 <msh794000@gmail.com>
2021-06-06 23:13:51 +0900

    푸시

---
+++ src/components_mobile/Content3.vue
@@ -0,0 +1,150 @@
+<template>
+  <div class="content3Main_m">
+    <div>
+      <div class="fonts">
+        <p>Our Goal</p>
+        <span>With more than 20 years of know-how,<br>we will be your business success partner.</span>
+      </div>
+      <div class="contentBox">
+        <div>
+          <div class="boxItem">
+            <div>
+              <div class="icon1" alt="icon1"></div>
+              <p>Analysis</p>
+            </div>
+            <p>기업 전체의 비즈니스를 한 눈에 볼 수 있게 <br> 함으로써 기업내의 자원과 노력을 효과적으로 <br> 배분할 수 있게 합니다.</p>
+          </div>
+          <div class="boxItem">
+            <div>
+              <div class="icon2" alt="icon2"></div>
+              <p>Optimization</p>
+            </div>
+            <p>현재를 위한 사업 최적화, 내일을 위한 사업기회 발굴 <br>등 균형을 맞출 수 있게 합니다.</p>
+          </div>
+        </div>
+        <div>
+          <div class="boxItem">
+            <div>
+              <div class="icon3" alt="icon3"></div>
+              <p>Insight</p>
+            </div>
+            <p>아웃사이드 (고객, 시장, 유통채널) 의 요구분석을<br>이해하고 이들의 시각을 통해 인사이드 (제품, <br>지식, 자원)을 볼 수 있게 합니다.</p>
+          </div>
+          <div class="boxItem">
+            <div>
+              <div class="icon4" alt="icon4"></div>
+              <p>Know-how</p>
+            </div>
+            <p>20년 이상 해당분야 노하우 및 실적을 보유한 <br>전문위원을 통해 단순 가이드가 아닌 실제 사례를<br>통한 정확한 진단과 분석을 제시합니다.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+
+
+export default {
+  components: {
+  }
+}
+</script>
+
+<style lang="scss">
+
+
+  .content3Main_m{
+    width: 100%;
+    height: 2444px;
+    background-image: url("../assets/content3/back.png");
+    background-repeat: no-repeat;
+    padding-top: 69px;
+    padding-left: 70px;
+
+    >div{
+      margin: 0 auto;
+      max-width: 1200px;
+      width: 100%;
+      // display: flex;
+      align-items: flex-start;
+      justify-content: space-between;
+    }
+
+    .fonts{
+      margin-bottom: 195px;
+      p,span{
+        color: #0d1741;
+        font-weight: bold;
+      }
+
+      p{
+        font-family: GmarketSansTTFBold ,sans-serif;
+        font-size: 4.6875rem;
+      }
+
+      span{
+        font-family : GmarketSansTTFMedium ,sans-serif;
+        display: block;
+        font-size: 2.5rem;
+      }
+    }
+    
+    .contentBox{
+      max-width: 1130px;
+      width: 100%;
+      // box-shadow:0px 30px 45px #0d1741;
+      >div{
+        width: 100%;
+        position: relative;
+        display: flex;
+        flex-flow: wrap;
+        align-items: center;
+      }
+
+      .boxItem{
+        // max-width: 1130px;
+        width: 100%;
+        height: 460px;
+        background: #101a44;
+        border: 1px solid #212c57;
+        padding-left: 80px;
+        padding-top: 65px;
+
+        >div{
+          display: flex;
+          margin-bottom: 20px;
+
+          >div{
+            margin-right: 20px; 
+            width: 110px;
+            height: 110px;
+            background-repeat: no-repeat;
+            
+          }
+
+          .icon1{background-image:url('../assets/mobile/content3/1.png')}
+          .icon2{background-image:url('../assets/mobile/content3/2.png')}
+          .icon3{background-image:url('../assets/mobile/content3/3.png')}
+          .icon4{background-image:url('../assets/mobile/content3/4.png')}
+
+          p{
+            // @import url(../assets/fonts/GmarketSansTTF/GmarketSansTTFMedium.ttf);
+            font-family: GmarketSansTTFMedium;
+            color: white;
+            font-size: 4.375rem;
+          }
+        }
+
+        >p{
+          @import url(https://fonts.googleapis.com/earlyaccess/notosanskr.css);
+          font-family: 'Noto Sans CJK KR', Noto Sans KR, sans-serif;
+          color:#c0c9ef;
+          font-size: 2.875rem;
+        }
+      }
+    }
+  }
+</style>
---
+++ src/components_mobile/TopNav.vue
@@ -0,0 +1,153 @@
+<template>
+  <div class="Navs_m">
+    <div class="NavLogo">
+      <img src="../assets/mobile/mobile_logo.png" alt="Logo">
+    </div>
+    <div class="NavButton" @click="open">
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+    </div>
+    <div class="NavPop">
+      <div>
+        <img @click="close" src="../assets/mobile/nav/close_button.png" alt="close">
+        <p @click="scroll(1)">About</p> 
+        <p @click="scroll(2)">Business area</p> 
+        <p @click="scroll(3)">Target</p> 
+        <p @click="scroll(4)">Our goal</p> 
+        <p @click="scroll(5)">Works</p> 
+        <p @click="scroll(6)">Partners</p> 
+        <p @click="scroll(7)">Contact</p> 
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+// import Logo from "@/assets/logo.png"
+
+export default {
+  components: {
+    // Logo
+  },
+
+  methods:{
+    open(){
+      document.getElementsByClassName("NavPop")[0].style.display = "flex";
+    },
+
+    close(){
+      document.getElementsByClassName("NavPop")[0].style.display = "none";
+    },
+
+    scroll(key){
+      let top = 0;
+      switch (key) {
+        case 1:
+          top = 0
+          break;
+        case 2:
+          top = 800
+          break;
+        case 3:
+          top = 1600
+          break;
+        case 4:
+          top = 2736
+          break;
+        case 5:
+          top = 3235
+          break;
+        case 6:
+          top = 5465
+          break;
+      
+        case 7:
+          top = 5465    
+          break;
+
+        default:
+          break;
+      }
+       window.scrollTo({
+        top: top,
+        left: 0,
+        behavior: 'smooth'
+      });
+    }
+  }
+}
+</script>
+
+<style  lang="scss">
+  .Navs_m{
+    max-width: 1200px;
+    width: 100%;
+    z-index: 10;
+    position: absolute;
+    top: 0;
+
+
+    .NavLogo{
+      position: absolute; 
+      top: 70px;
+      left: 70px;
+      >img{
+        width: 450px;     
+      }
+    }
+
+    .NavButton{ 
+      display: none;
+      width: 40px;
+      height: 40px;
+      flex-flow: wrap;
+      position: fixed;
+      top: 70px;
+      right: 70px;
+      z-index: 100;
+
+      >p{
+        width: 100%;
+        height: 5px;
+        background: white;
+        border-radius: 10px;
+      }
+    } 
+    .NavButton:hover{
+      cursor: pointer;
+    }
+    .NavPop{
+      background: rgba(0, 0, 0, .6);
+      width: 100%;
+      height: 100%;
+      position: relative;
+      height: 10000px;
+      z-index: 1000;
+      display: none;
+      justify-content: flex-end;
+
+      >div{
+        width: 690px;
+        height: 100%;
+        background: #0d1741;
+        padding-left: 75px;
+        padding-top: 70px;
+
+        >img{
+          margin-bottom: 100px;
+          padding-left: 465px;
+        }
+
+        >p{
+          font-size: 3.125rem;
+          color: white;
+          font-family: 'Objektiv Mk2 W03 Medium' , sans-serif;
+          margin-bottom: 50px;
+        }
+      }
+    }
+  }
+</style>
